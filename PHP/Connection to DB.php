d12001652130d3f56f479a0992af0ae5c76cad5c
Abhiram Kalyan <78918000+abhiram-kalyan@users.noreply.github.com>
2021-04-04 15:19:31 +0530

    Added Php files

---
+++ PHP/Connection to DB.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task1</title>
+    <style type="text/css">
+        table,th,td
+        {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th
+        {
+            text-align: left;
+        }
+        td
+        {
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>RNo</th>
+            <th>SName</th>
+            <th>Total</th>
+            <th>Percentage</th>
+            <th>Result</th>
+        </tr>
+        <tr>
+            <th><?php echo $_POST["rno"]; ?></th>
+            <th><?php echo $_POST["sname"]; ?></th>
+            <th><?php echo $_POST["s1"]+$_POST["s2"]+$_POST["s3"]; ?></th>
+            <th><?php echo ($_POST["s1"]+$_POST["s2"]+$_POST["s3"])/3; ?></th>
+            <th><?php 
+               $t =($_POST["s1"]+$_POST["s2"]+$_POST["s3"])/3;
+               if ($t > 60) {
+                   echo "First Class";
+               }
+               
+               elseif ($t > 50) {
+                   echo "Second Class";
+               }
+               elseif ($t > 30) {
+                   echo "Third Class";
+               }
+               else{
+                   echo "Fail";
+               }
+            ?></th>
+            
+        </tr>
+    </table>
+
+</body>
+</html>
